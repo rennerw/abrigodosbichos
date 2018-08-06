08cc6dc98532107bffc64189b166f3d4aa0bf86c
Tonzera Tutoriais <rennerwellinghton@gmail.com>
2018-08-06 09:19:23 -0300

    primero commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,187 @@
+<!doctype HTML>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title> Abrigo Dos Bichos</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!--CF core CSS -->
+        <link href="css/estilo.css" rel ="stylesheet">
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom fonts for this template -->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
+        <link href="css/simple-line-icons.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="css/resume.min.css" rel="stylesheet">
+
+        <script  src="js/main.js" charset="UTF-8"></script></head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Abrigo dos Bichos</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/logosite.gif" alt="">
+        </span>
+      </a>
+      <br>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+
+            <a class="nav-link js-scroll-trigger cor-primaria" href="#about"> <img id = "dogs" src="img/dog1.png" alt="cachorro">
+                <br>Sobre</a>
+          </li>
+          
+
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger cor-primaria" href="#skills"> <img id="dogs" src="img/dog2.png" alt="cachorro">
+                <br>Doenças</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger cor-primaria" href="#interests"> <img id="dogs" src="img/dog5.png" alt="cachorro"><br>Parceiros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger cor-primaria" href="#experience"><img id ="dogs" src="img/dog4.png" alt="cachorro">
+                <br>Contato</a>
+          </li>
+          
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid p-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+          <h1 class="mb-0">
+            <span class="text-primary">Abrigo</span> <span class="cor-secundaria">dos Bichos</span> <img id = "dogs" src="img/dog1.png" alt="cachorro">
+          </h1>
+          <div class="subheading mb-5">Rua Padre João Crippa 1.065 - Sala 103 - Campo Grande - MS
+          </div>
+          <p class="mb-5">A Sociedade de Proteção e Bem-Estar Animal “ Abrigo dos Bichos " é uma Associação Civil de Direito Privado, de caráter sócio-ambientalista, sem fins lucrativos, de duração indeterminada, de âmbito nacional, constituída por pessoas interessadas na proteção e bem-estar, com sede e foro na cidade de Campo Grande, capital do Estado de Mato Grosso do Sul, à Rua Padre João Crippa 1.065 - Sala 103, inscrita no CNPJ sob nº 05.108.286/0001-47. 
+        A missão do “Abrigo dos Bichos” é: “Preservar a Saúde Pública Respeitando a Vida Animal”.</p>
+          <ul class="list-inline list-social-icons mb-1">
+            <li class="list-inline-item" style="min-height: 4em; min-width: 4em;">
+                <a href="https://pt-br.facebook.com/AbrigoDosBichos/" target="_blank">
+              <i class="devicon-facebook-plain colored"></i>
+            </a>
+          </ul>
+        </div>
+      </section>
+
+            
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+        <div class="my-auto">
+          <h2 class="mb-5 cor-secundaria">Doenças<img id="dogs" src="img/dog2.png" alt="cachorro"></h2>
+          <div class="row">
+            <div class="col-md-12 doencas">
+              <ul>
+                <li>
+                  <a href="#">Leishmaniose</a>
+                  <p class="descricao">
+                        É causada por parasitas, protozoários conhecidos por “Leishmania”, que invadem e se reproduzem dentro das células que fazem parte do sistema imunológico (macrófagos). Esta doença pode se manifestar de duas formas: visceral ou cutânea.<br>
+                        Sintomas:<br>
+                        Úlceras.<br>
+                        Falta de apetite.<br>
+                        Perda de peso.<br>
+                        Lesões oculares (tipo queimaduras).<br>
+                        Atrofia muscular.<br>
+                        Crescimento exagerado das unhas.<br>
+                        Problemas nos rins, fígado e baço que levam à morte (em um estágio mais avançado).
+                    </p>
+                </li>
+                <li>
+                  <a href="#">Raiva Canina</a>
+                  <p class="descricao">
+                        A doença causada por vírus é uma zoonose (transmitível para qualquer mamífero) praticamente fatal. O contágio acontece a partir do contato com secreção, modida ou sangue de animais já contaminados. Possui diferentes tipos de fases e formas, os sintomas principais são: mudança do comportamento, inquietação, desobediência, muita salivação e perda de apetite. Devido a vacina o vírus da raiva está praticamente erradicado, mas a alta gravidade dele o impede de ser deixado de lado. 
+                        Fonte: Canal do Pet - iG @ http://canaldopet.ig.com.br/cuidados/saude/2016-09-26/doencas-cachorros.html
+                    </p>
+                </li>
+                <li>
+                  <a href="#">Cinomose</a>
+                  <p class="descricao">
+                        A cinomose está entre uma das doenças de cachorros mais graves . Isso porque ela atinge principalmente os filhotes, antes de completarem um mês de vida, quando ainda não possuem um sistema imunológico muito resistente. Os principais sintomas são febre, perda de apetite, corrimental ocular e nasal, diarreia, vômito, tiques nervosos e paralisias. Há prevenção por meio de vacinas, mas não há um tratamento em si, esse é feito por meio da amenização dos sintomas para permitir que o organismo do animal lute apenas contra o vírus. 
+Fonte: Canal do Pet - iG @ http://canaldopet.ig.com.br/cuidados/saude/2016-09-26/doencas-cachorros.html
+                    </p>
+                </li>
+                <li>
+                  <a href="#">Leptospirose Canina</a>
+                  <p class="descricao">
+                        A leptospirose é uma doença transmitida através da urina de ratos contaminados , por isso é conhecida também como a "doença do rato". Uma vez no organismo canino a bactéria terá ações diferentes em cada um. Os sintomas mais comuns são hematomas e machucados na boca e na pele, falta de apetite, vômito, diarreia e febre. As doses da vacina de prevenção devem ser aplicadas a cada seis meses. 
+Fonte: Canal do Pet - iG @ http://canaldopet.ig.com.br/cuidados/saude/2016-09-26/doencas-cachorros.html
+                    </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+        <div class="my-auto">
+          <h2 class="mb-5 cor-secundaria">Parceiros <img id="dogs" src="img/dog5.png" alt="cachorro"> </h2>
+          <p><strong>Nossos parceiros são:</strong></p>
+          <ul>
+            <li><a href="https://about.me/rennerw" target="_blank">Wellinghton</a></li>
+          </ul>
+        </div>
+      </section>
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <div class="my-auto">
+          <h2 class="mb-5 cor-secundaria">Contato <img id = "dogs" src="img/dog4.png" alt="cachorro"></h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            {{ Form::open(array('action' => 'ContactController@getContactUsForm')) }} 
+
+            <ul class="errors">
+            @foreach($errors->all('<li>:message</li>') as $message)
+
+            @endforeach
+            </ul>
+
+            <div class="form-group">
+            <input class="form-control centralizado" type="text" name="nome" placeholder="Nome" maxlength="20" required>
+            <br>
+            <br>
+            {{ Form:: textarea ('message', '', array('placeholder' => 'Diga algo', 'class' => 'form-control', 'id' => 'message', 'rows' => '4', 'required' )) }}
+            </div>
+
+
+
+            </div>
+            <div class="modal-footer">
+            {{ Form::submit('Enviar', array('class' => 'btn btn-primary centralizado')) }}
+            {{ Form:: close() }}
+
+      </section>
+      
+
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/resume.min.js"></script>
+</body>
+</html>
